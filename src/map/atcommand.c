missxantara/rAthena

commit d396736e00da95c3b42a5b2b878d8407f37ed43c

- Cleaned up a bit the format of skill_db.txt (that comma next to the skill name looks ugly if you ask me) - Corrected skill_db reading to properly trim the skill name/descs. - Added a mobid_db in map.c to handle mob lookups faster.
git-svn-id: http://svn.code.sf.net/p/rathena/svn/trunk@11943 54d463be-8e91-2dee-dedb-b68131a5f0ec

src/map/mob.c
@@ -1383,7 +1383,7 @@ static int mob_ai_sub_lazy(DBKey key,void * data,va_list ap)
 
 	nullpo_retr(0, md);
 
-	if(md->bl.type!=BL_MOB || md->bl.prev == NULL)
+	if(md->bl.prev == NULL)
 		return 0;
 
 	if (md->nd || (battle_config.mob_ai&0x20 && map[md->bl.m].users>0))
@@ -1443,7 +1443,7 @@ static int mob_ai_sub_lazy(DBKey key,void * data,va_list ap)
  *------------------------------------------*/
 static int mob_ai_lazy(int tid,unsigned int tick,int id,int data)
 {
-	map_foreachiddb(mob_ai_sub_lazy,tick);
+	map_foreachmob(mob_ai_sub_lazy,tick);
 	return 0;
 }
 

src/map/skill.c
@@ -10821,8 +10821,8 @@ static bool skill_parse_row_skilldb(char* split[], int columns, int current)
 	else
 		skill_db[i].skill_type = 0;
 	skill_split_atoi(split[14],skill_db[i].blewcount);
-	safestrncpy(skill_db[i].name, split[15], sizeof(skill_db[i].name));
-	safestrncpy(skill_db[i].desc, split[16], sizeof(skill_db[i].desc));
+	safestrncpy(skill_db[i].name, trim(split[15]), sizeof(skill_db[i].name));
+	safestrncpy(skill_db[i].desc, trim(split[16]), sizeof(skill_db[i].desc));
 
 	return true;
 }

src/map/map.c
@@ -96,6 +96,7 @@ char *MSG_CONF_NAME;
 // ‹É—Í static‚Åƒ?ƒJƒ‹‚É?‚ß‚é
 static DBMap* id_db=NULL; // int id -> struct block_list*
 static DBMap* pc_db=NULL; // int id -> struct map_session_data*
+static DBMap* mobid_db=NULL; // int id -> struct mob_data*
 static DBMap* map_db=NULL; // unsigned int mapindex -> struct map_data*
 static DBMap* nick_db=NULL; // int char_id -> struct charid2nick* (requested names of offline characters)
 static DBMap* charid_db=NULL; // int char_id -> struct map_session_data*
@@ -1643,7 +1644,9 @@ void map_addiddb(struct block_list *bl)
 		TBL_PC* sd = (TBL_PC*)bl;
 		idb_put(pc_db,sd->bl.id,sd);
 		idb_put(charid_db,sd->status.char_id,sd);
-	}
+	} else if (bl->type == BL_MOB)
+		idb_put(mobid_db,bl->id,bl);
+
 	idb_put(id_db,bl->id,bl);
 }
 
@@ -1659,7 +1662,8 @@ void map_deliddb(struct block_list *bl)
 		TBL_PC* sd = (TBL_PC*)bl;
 		idb_remove(pc_db,sd->bl.id);
 		idb_remove(charid_db,sd->status.char_id);
-	}
+	} else if (bl->type == BL_MOB)
+		idb_remove(mobid_db,bl->id);
 	idb_remove(id_db,bl->id);
 }
 
@@ -1764,9 +1768,9 @@ struct map_session_data * map_id2sd(int id)
 }
 
 struct mob_data * map_id2md(int id)
-{// just a id2bl lookup because there's no mob_db
+{
 	if (id <= 0) return NULL;
-	return (struct mob_data*)map_id2bl(id);
+	return (struct mob_data*)idb_get(mobid_db,id);
 }
 
 struct npc_data * map_id2nd(int id)
@@ -1906,6 +1910,14 @@ void map_foreachpc(int (*func)(DBKey,void*,va_list),...)
 	va_list ap;
 	va_start(ap,func);
 	pc_db->vforeach(pc_db,func,ap);
+	va_end(ap);
+}
+
+void map_foreachmob(int (*func)(DBKey,void*,va_list),...)
+{
+	va_list ap;
+	va_start(ap,func);
+	mobid_db->vforeach(mobid_db,func,ap);
 	va_end(ap);
 }
 
@@ -3113,6 +3125,7 @@ void do_final(void)
 
 	id_db->destroy(id_db, NULL);
 	pc_db->destroy(pc_db, NULL);
+	mobid_db->destroy(mobid_db, NULL);
 	nick_db->destroy(nick_db, nick_db_final);
 	charid_db->destroy(charid_db, NULL);
 
@@ -3291,6 +3304,7 @@ int do_init(int argc, char *argv[])
 
 	id_db = idb_alloc(DB_OPT_BASE);
 	pc_db = idb_alloc(DB_OPT_BASE);	//Added for reliable map_id2sd() use. [Skotlex]
+	mobid_db = idb_alloc(DB_OPT_BASE);	//Added to lower the load of the lazy mob ai. [Skotlex]
 	map_db = uidb_alloc(DB_OPT_BASE);
 	nick_db = idb_alloc(DB_OPT_BASE);
 	charid_db = idb_alloc(DB_OPT_BASE);

src/map/map.h
@@ -1335,6 +1335,7 @@ void map_addiddb(struct block_list *);
 void map_deliddb(struct block_list *bl);
 struct map_session_data** map_getallusers(int *users);
 void map_foreachpc(int (*func)(DBKey,void*,va_list),...);
+void map_foreachmob(int (*func)(DBKey,void*,va_list),...);
 int map_foreachiddb(int (*)(DBKey,void*,va_list),...);
 struct map_session_data * map_nick2sd(const char*);
 

commit 009a739cb890a7edfd6cff193a69fe7e6b7a48fe

Fixed mob AI code iterating over non-mob objects without checking, when monster_ai flag 0x20 is set (caused by r11943) Cleaned up macros that wrap unit_stop_attack() (bugreport:357)
git-svn-id: http://svn.code.sf.net/p/rathena/svn/trunk@11957 54d463be-8e91-2dee-dedb-b68131a5f0ec

src/map/atcommand.c
@@ -6033,10 +6033,10 @@ int atcommand_partyoption(const int fd, struct map_session_data* sd, const char*
 
 	if(!message || !*message || sscanf(message, "%15s %15s", w1, w2) < 2)
 	{
-		clif_displaymessage(fd, "Command usage: @changeoption <pickup share: yes/no> <item distribution: yes/no>");
+		clif_displaymessage(fd, "Command usage: @partyoption <pickup share: yes/no> <item distribution: yes/no>");
 		return -1;
 	}
-
+	
 	option = (config_switch(w1)?1:0)|(config_switch(w2)?2:0);
 
 	//Change item share type.
@@ -6390,12 +6390,6 @@ int atcommand_pettalk(const int fd, struct map_session_data* sd, const char* com
 {
 	char mes[100],temp[100];
 	struct pet_data *pd;
-	const char* emo[] = {	 "/!", "/?", "/ho", "/lv", "/swt", "/ic", "/an", "/ag", "/$", "/...",
-				"/scissors", "/rock", "/paper", "/korea", "/lv2", "/thx", "/wah", "/sry", "/heh", "/swt2",
-				"/hmm", "/no1", "/??", "/omg", "/O", "/X", "/hlp", "/go", "/sob", "/gg",
-				"/kis", "/kis2", "/pif", "/ok", "-?-", "-?-", "/bzz", "/rice", "/awsm", "/meh",
-				"/shy", "/pat", "/mp", "/slur", "/com", "/yawn", "/grat", "/hp", "/philippines", "/usa",
-				"/indonesia", "/brazil", "/fsh", "/spin", "/sigh", "/dum", "/crwd", "/desp", "/dice", NULL };
 
 	nullpo_retr(-1, sd);
 
@@ -6415,13 +6409,22 @@ int atcommand_pettalk(const int fd, struct map_session_data* sd, const char* com
 		return -1;
 	}
 
-	if (message[0] == '/') {
+	if (message[0] == '/')
+	{// pet emotion processing
+		const char* emo[] = {
+			"/!", "/?", "/ho", "/lv", "/swt", "/ic", "/an", "/ag", "/$", "/...",
+			"/scissors", "/rock", "/paper", "/korea", "/lv2", "/thx", "/wah", "/sry", "/heh", "/swt2",
+			"/hmm", "/no1", "/??", "/omg", "/O", "/X", "/hlp", "/go", "/sob", "/gg",
+			"/kis", "/kis2", "/pif", "/ok", "-?-", "-?-", "/bzz", "/rice", "/awsm", "/meh",
+			"/shy", "/pat", "/mp", "/slur", "/com", "/yawn", "/grat", "/hp", "/philippines", "/usa",
+			"/indonesia", "/brazil", "/fsh", "/spin", "/sigh", "/dum", "/crwd", "/desp", "/dice"
+		};
 		int i;
-		for (i = 0; emo[i] != NULL; i++) {
-			if (!stricmp(message, emo[i])) {
-				clif_emotion(&pd->bl, i);
-				return 0;
-			}
+		ARR_FIND( 0, ARRAYLENGTH(emo), i, stricmp(message, emo[i]) == 0 );
+		if( i < ARRAYLENGTH(emo) )
+		{
+			clif_emotion(&pd->bl, i);
+			return 0;
 		}
 	}
 

src/map/intif.c
@@ -1032,6 +1032,7 @@ int intif_parse_LoadGuildStorage(int fd)
 	gstor=guild2storage(guild_id);
 	if(!gstor) {
 		ShowWarning("intif_parse_LoadGuildStorage: error guild_id %d not exist\n",guild_id);
+		return 1;
 	}
 	if (gstor->storage_status == 1) { // Already open.. lets ignore this update
 		ShowWarning("intif_parse_LoadGuildStorage: storage received for a client already open (User %d:%d)\n", sd->status.account_id, sd->status.char_id);

src/map/mercenary.h
@@ -55,7 +55,7 @@ int merc_hom_hungry_timer_delete(struct homun_data *hd);
 int merc_hom_change_name(struct map_session_data *sd,char *name);
 int merc_hom_change_name_ack(struct map_session_data *sd, char* name, int flag);
 #define merc_stop_walking(hd, type) unit_stop_walking(&(hd)->bl, type)
-#define merc_stop_attack(hd) { if((hd)->ud.attacktimer != -1) unit_stop_attack(&(hd)->bl); hd->ud.target = 0; }
+#define merc_stop_attack(hd) unit_stop_attack(&(hd)->bl)
 int merc_hom_increase_intimacy(struct homun_data * hd, unsigned int value);
 int merc_hom_decrease_intimacy(struct homun_data * hd, unsigned int value);
 int merc_skill_tree_get_max(int id, int b_class);

src/map/mob.c
@@ -1454,7 +1454,7 @@ static int mob_ai_hard(int tid,unsigned int tick,int id,int data)
 {
 
 	if (battle_config.mob_ai&0x20)
-		map_foreachiddb(mob_ai_sub_lazy,tick);
+		map_foreachmob(mob_ai_sub_lazy,tick);
 	else
 		clif_foreachclient(mob_ai_sub_foreachclient,tick);
 

src/map/mob.h
@@ -178,7 +178,7 @@ void mob_revive(struct mob_data *md, unsigned int hp);
 void mob_heal(struct mob_data *md,unsigned int heal);
 
 #define mob_stop_walking(md, type) unit_stop_walking(&(md)->bl, type)
-#define mob_stop_attack(md) { if((md)->ud.attacktimer != -1) unit_stop_attack(&(md)->bl); }
+#define mob_stop_attack(md) unit_stop_attack(&(md)->bl)
 
 void mob_clear_spawninfo();
 int do_init_mob(void);

src/map/party.c
@@ -437,7 +437,7 @@ int party_member_leaved(int party_id, int account_id, int char_id)
 		}
 	}
 
-	if( sd && sd->status.party_id==party_id && sd->status.char_id == char_id )
+	if( sd && sd->status.party_id == party_id && sd->status.char_id == char_id )
 	{
 		sd->status.party_id = 0;
 		sd->state.party_sent = 0;

src/map/pc.h
@@ -119,7 +119,7 @@ enum {
 #define pc_maxparameter(sd)   ( (sd)->class_&JOBL_BABY ? battle_config.max_baby_parameter : battle_config.max_parameter )
 
 #define pc_stop_walking(sd, type) unit_stop_walking(&(sd)->bl, type)
-#define pc_stop_attack(sd) { if((sd)->ud.attacktimer != -1) { unit_stop_attack(&(sd)->bl); (sd)->ud.target = 0; } }
+#define pc_stop_attack(sd) unit_stop_attack(&(sd)->bl)
 
 //Weapon check considering dual wielding.
 #define pc_check_weapontype(sd, type) ((type)&((sd)->status.weapon < MAX_WEAPON_TYPE? \

src/map/pet.h
@@ -59,7 +59,7 @@ int pet_recovery_timer(int tid,unsigned int tick,int id,int data); // [Valaris]
 int pet_heal_timer(int tid,unsigned int tick,int id,int data); // [Valaris]
 
 #define pet_stop_walking(pd, type) unit_stop_walking(&(pd)->bl, type)
-#define pet_stop_attack(pd) { if((pd)->ud.attacktimer != -1) unit_stop_attack(&(pd)->bl); }
+#define pet_stop_attack(pd) unit_stop_attack(&(pd)->bl)
 
 int read_petdb(void);
 int do_init_pet(void);

src/map/skill.c
@@ -4567,7 +4567,7 @@ int skill_castend_nodamage_id (struct block_list *src, struct block_list *bl, in
 			if( (su)
 			&&  (sg = su->group)
 			&&  (sg->src_id == src->id || map_flag_vs(bl->m))
-			&&	(skill_get_inf2(sg->skill_id)&INF2_TRAP) )
+			&&  (skill_get_inf2(sg->skill_id)&INF2_TRAP) )
 			{	// prevent picking up expired traps
 				if( !(sg->unit_id == UNT_USED_TRAPS || (sg->unit_id == UNT_ANKLESNARE && sg->val2 != 0 )) )
 				{